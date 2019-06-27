ad254f6d56312fd1813e3440866ca1d84279672f
Jay Heng <jie.heng@nxp.com>
2019-06-27 16:45:30 +0800

    apps/dhrystone_rt1010 #2 Bsp
    - Used bsp files from RT1010 bringup package/hello_world demo, rather than led_blinky demo
    
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- apps/dhrystone_imxrt1011/bsp/src/pin_mux.c
+++ apps/dhrystone_imxrt1011/bsp/src/pin_mux.c
@@ -40,8 +40,10 @@ void BOARD_InitBootPins(void) {
 BOARD_InitPins:
 - options: {callFromInitBoot: 'true', coreID: core0, enableClock: 'true'}
 - pin_list:
-  - {pin_num: '1', peripheral: GPIO1, signal: 'gpiomux_io, 11', pin_signal: GPIO_11, slew_rate: Slow, software_input_on: Disable, open_drain: Disable, speed: MHZ_150,
-    drive_strength: R0_4, pull_keeper_select: Keeper, pull_keeper_enable: Enable, pull_up_down_config: Pull_Down_100K_Ohm, hysteresis_enable: Disable}
+  - {pin_num: '3', peripheral: LPUART1, signal: RX, pin_signal: GPIO_09, slew_rate: Slow, software_input_on: Disable, open_drain: Disable, speed: MHZ_150, drive_strength: R0_4,
+    pull_keeper_select: Keeper, pull_keeper_enable: Enable, pull_up_down_config: Pull_Down_100K_Ohm, hysteresis_enable: Disable}
+  - {pin_num: '2', peripheral: LPUART1, signal: TX, pin_signal: GPIO_10, slew_rate: Slow, software_input_on: Disable, open_drain: Disable, speed: MHZ_150, drive_strength: R0_4,
+    pull_keeper_select: Keeper, pull_keeper_enable: Enable, pull_up_down_config: Pull_Down_100K_Ohm, hysteresis_enable: Disable}
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS ***********
  */
 
@@ -55,14 +57,23 @@ void BOARD_InitPins(void) {
   CLOCK_EnableClock(kCLOCK_Iomuxc);           /* iomuxc clock (iomuxc_clk_enable): 0x03U */
 
   IOMUXC_SetPinMux(
-      IOMUXC_GPIO_00_GPIOMUX_IO00,            /* GPIO_11 is configured as GPIOMUX_IO11 */
+      IOMUXC_GPIO_09_LPUART1_RXD,             /* GPIO_09 is configured as LPUART1_RXD */
       0U);                                    /* Software Input On Field: Input Path is determined by functionality */
-  IOMUXC_GPR->GPR26 = ((IOMUXC_GPR->GPR26 &
-    (~(IOMUXC_GPR_GPR26_GPIO_SEL_MASK)))      /* Mask bits to zero which are setting */
-      | IOMUXC_GPR_GPR26_GPIO_SEL(0x00U)      /* Select GPIO1 or GPIO2: 0x00U */
-    );
+  IOMUXC_SetPinMux(
+      IOMUXC_GPIO_10_LPUART1_TXD,             /* GPIO_10 is configured as LPUART1_TXD */
+      0U);                                    /* Software Input On Field: Input Path is determined by functionality */
   IOMUXC_SetPinConfig(
-      IOMUXC_GPIO_00_GPIOMUX_IO00,            /* GPIO_11 PAD functional properties : */
+      IOMUXC_GPIO_09_LPUART1_RXD,             /* GPIO_09 PAD functional properties : */
+      0x10A0U);                               /* Slew Rate Field: Slow Slew Rate
+                                                 Drive Strength Field: R0/4
+                                                 Speed Field: fast(150MHz)
+                                                 Open Drain Enable Field: Open Drain Disabled
+                                                 Pull / Keep Enable Field: Pull/Keeper Enabled
+                                                 Pull / Keep Select Field: Keeper
+                                                 Pull Up / Down Config. Field: 100K Ohm Pull Down
+                                                 Hyst. Enable Field: Hysteresis Disabled */
+  IOMUXC_SetPinConfig(
+      IOMUXC_GPIO_10_LPUART1_TXD,             /* GPIO_10 PAD functional properties : */
       0x10A0U);                               /* Slew Rate Field: Slow Slew Rate
                                                  Drive Strength Field: R0/4
                                                  Speed Field: fast(150MHz)

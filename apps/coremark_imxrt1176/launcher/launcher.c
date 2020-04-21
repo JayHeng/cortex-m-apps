0b339ed27f1c48485c30d19b9f03b946caf36744
Jay Heng <jie.heng@nxp.com>
2020-04-21 15:41:35 +0800

    apps/coremark_rt1170 #17 cm4 launcher works now
    - Refer to code in \SDK_2.6.1_MIMXRT1170-EVK_PRC2_3\boards\evkmimxrt1170\driver_examples\mu\polling\core0
    
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- apps/coremark_imxrt1176/launcher/launcher.c
+++ apps/coremark_imxrt1176/launcher/launcher.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include "fsl_device_registers.h"
 #include "cm4_coremark.h"
+#include "fsl_soc_src.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -45,6 +46,26 @@ static void init_cm4_tcm_ecc(void)
 
 }
 
+static void boot_cm4_app(void)
+{
+    IOMUXC_LPSR_GPR->GPR0 = IOMUXC_LPSR_GPR_GPR0_CM4_INIT_VTOR_LOW(APP_START);
+    IOMUXC_LPSR_GPR->GPR1 = IOMUXC_LPSR_GPR_GPR1_CM4_INIT_VTOR_HIGH(APP_START >> 16);
+
+    /* If CM4 is already running (released by debugger), then reset the CM4.
+       If CM4 is not running, release it. */
+    if ((SRC->SCR & SRC_SCR_BT_RELEASE_M4_MASK) != 0)
+    {
+        SRC_AssertSliceSoftwareReset(SRC, kSRC_M4CoreSlice);
+        while (kSRC_SliceResetInProcess == SRC_GetSliceResetState(SRC, kSRC_M4CoreSlice))
+        {
+        }
+    }
+    else
+    {
+        SRC_ReleaseCoreReset(SRC, kSRC_CM4Core);
+    }
+}
+
 /*!
  * @brief Main function
  */
@@ -53,13 +74,18 @@ int main(void)
     enable_cm4_tcm_ecc();
     init_cm4_tcm_ecc();
 
+#if defined(__DCACHE_PRESENT) && (__DCACHE_PRESENT == 1U)
+    SCB_CleanInvalidateDCache_by_Addr((void *)APP_START, APP_LEN);
+#endif
+
     // Copy image to RAM.
     memcpy((void *)APP_START, cm4_app_code, APP_LEN);
-    
-    IOMUXC_LPSR_GPR->GPR0 = 0x0000;
-    IOMUXC_LPSR_GPR->GPR1 = 0x1FFE;
 
-    SRC->SCR |= SRC_SCR_BT_RELEASE_M4_MASK;
+#if defined(__DCACHE_PRESENT) && (__DCACHE_PRESENT == 1U)
+    SCB_CleanInvalidateDCache_by_Addr((void *)APP_START, APP_LEN);
+#endif
+    
+    boot_cm4_app();
 
     while (1)
     {

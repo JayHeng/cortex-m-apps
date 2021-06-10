b8f5ece0cee73399a0347ce37ff38e24907fe7a4
Jay Heng <jie.heng@nxp.com>
2021-06-10 11:14:19 +0800

    apps/autobaud_imxrt1010 #1 Initial printf
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

---
+++ apps/autobaud_imxrt1011/src/main.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "board.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    /* Init board hardware. */
+    BOARD_InitPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+
+    PRINTF("Autobaud test success\r\n");
+
+    while (1)
+    {
+    }
+}

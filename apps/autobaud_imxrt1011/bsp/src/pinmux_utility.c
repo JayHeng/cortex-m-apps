8ada3d80fdc57ec4eec9eac8a52635397f1f8133
Jay Heng <jie.heng@nxp.com>
2021-06-10 22:48:21 +0800

    apps/autobaud_imxrt1010 #2 done
    - BOARD_BootClockRUN(), systick - fail
    - clock_setup(),        systick - fail
    - BOARD_BootClockRUN(),   pit   - fail
    - clock_setup(),          pit   - success
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

---
+++ apps/autobaud_imxrt1011/bsp/src/pinmux_utility.c
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2014-2016 Freescale Semiconductor, Inc.
+ * Copyright 2016-2018 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+#include "fsl_device_registers.h"
+#include "fsl_iomuxc.h"
+#include "fsl_gpio.h"
+#include "autobaud.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+//! @brief UART autobaud port irq configurations
+#define GPIO_IRQC_INTERRUPT_ENABLED_PRIORITY 1
+#define GPIO_IRQC_INTERRUPT_RESTORED_PRIORITY 0
+
+/*====================== LPUART IOMUXC Definitions ===========================*/
+#define UART1_RX_IOMUXC_MUX_FUNC    IOMUXC_GPIO_09_LPUART1_RXD
+#define UART1_RX_IOMUXC_MUX_GPIO    IOMUXC_GPIO_09_GPIOMUX_IO09
+#define UART1_RX_GPIO_BASE          GPIO1
+#define UART1_RX_GPIO_PIN_NUM       9 // gpiomux.io[9], sel gpio1/gpio2 via iomuxc_gpr26
+#define UART1_RX_GPIO_IRQn          GPIO1_Combined_0_15_IRQn
+#define UART1_RX_GPIO_IRQHandler    GPIO1_Combined_0_15_IRQHandler
+
+#define UART1_TX_IOMUXC_MUX_FUNC    IOMUXC_GPIO_10_LPUART1_TXD
+#define UART1_TX_IOMUXC_PAD_DEFAULT 0x000010B0
+#define UART1_TX_GPIO_PIN_NUM       10 // gpiomux.io[10], sel gpio1/gpio2 via iomuxc_gpr26
+
+#define LPUART1_PAD_CTRL            (IOMUXC_SW_PAD_CTL_PAD_SRE(0) | IOMUXC_SW_PAD_CTL_PAD_DSE(6) | IOMUXC_SW_PAD_CTL_PAD_SPEED(2) | \
+                                     IOMUXC_SW_PAD_CTL_PAD_PKE(1) | IOMUXC_SW_PAD_CTL_PAD_PUE(1) | IOMUXC_SW_PAD_CTL_PAD_PUS(1))
+#define UART1_PULLUP_PAD_CTRL       (IOMUXC_SW_PAD_CTL_PAD_PKE(1) | IOMUXC_SW_PAD_CTL_PAD_PUS(2))
+
+////////////////////////////////////////////////////////////////////////////////
+// Variables
+////////////////////////////////////////////////////////////////////////////////
+
+//! this is to store the function pointer for calling back to the function that wants
+//! the UART RX instance pin that triggered the interrupt. This only supports 1 pin
+//! for UART0 because UART1 is on PORTC which does not support interrupts :(
+
+static pin_irq_callback_t s_pin_irq_func;
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+static inline void IOMUXC_RestoreDefault(uint32_t muxRegister,
+                                         uint32_t muxMode,
+                                         uint32_t inputRegister,
+                                         uint32_t inputDaisy,
+                                         uint32_t configRegister,
+                                         uint32_t configDefaultValue)
+{
+    *((volatile uint32_t *)muxRegister) = 0 /*Mux default value*/;
+    if (inputRegister)
+    {
+        *((volatile uint32_t *)inputRegister) = 0 /*Daisy defualt value*/;
+    }
+    if (configRegister)
+    {
+        *((volatile uint32_t *)configRegister) = configDefaultValue;
+    }
+}
+
+static inline void IOMUXC_SetUartAutoBaudPinMode(uint32_t muxRegister,
+                                                 uint32_t muxMode,
+                                                 uint32_t inputRegister,
+                                                 uint32_t inputDaisy,
+                                                 uint32_t configRegister,
+                                                 GPIO_Type *gpioBase,
+                                                 uint32_t pin)
+{
+    // Configure the UART RX pin to GPIO mode
+    IOMUXC_SetPinMux(muxRegister, muxMode, inputRegister, inputDaisy, configRegister, 0);
+
+    // Pull-up resistor enabled.
+    *((volatile uint32_t *)configRegister) |= UART1_PULLUP_PAD_CTRL;
+
+    // Configure UART RX pin to digital input mode.
+    gpioBase->GDIR &= (uint32_t) ~(1 << pin);
+}
+
+static inline void IOMUXC_SetUartPinMode(
+    uint32_t muxRegister, uint32_t muxMode, uint32_t inputRegister, uint32_t inputDaisy, uint32_t configRegister)
+{
+    IOMUXC_SetPinMux(muxRegister, muxMode, inputRegister, inputDaisy, configRegister, 0);
+    *(volatile uint32_t *)configRegister = LPUART1_PAD_CTRL;
+};
+
+/*!
+ * @brief Configure pinmux for uart module.
+ *
+ * This function only support switching default or gpio or fixed-ALTx mode on fixed pins
+ * (Although there are many ALTx-pinmux configuration choices on various pins for the same
+ * peripheral module)
+ */
+void uart_pinmux_config(pinmux_type_t pinmux)
+{
+    switch (pinmux)
+    {
+        case kPinmuxType_Default:
+            IOMUXC_RestoreDefault(UART1_TX_IOMUXC_MUX_FUNC, UART1_TX_IOMUXC_PAD_DEFAULT);
+            IOMUXC_RestoreDefault(UART1_TX_IOMUXC_MUX_FUNC, UART1_TX_IOMUXC_PAD_DEFAULT);
+            break;
+        case kPinmuxType_PollForActivity:
+            IOMUXC_SetUartAutoBaudPinMode(UART1_RX_IOMUXC_MUX_GPIO, UART1_RX_GPIO_BASE, UART1_RX_GPIO_PIN_NUM);
+            break;
+        case kPinmuxType_Peripheral:
+            // Enable pins for UART1.
+            IOMUXC_SetUartPinMode(UART1_RX_IOMUXC_MUX_FUNC);
+            IOMUXC_SetUartPinMode(UART1_TX_IOMUXC_MUX_FUNC);
+            break;
+        default:
+            break;
+    }
+}
+
+//! @brief this is going to be used for autobaud IRQ handling for UART1
+void UART1_RX_GPIO_IRQHandler(void)
+{
+    uint32_t interrupt_flag = (1U << UART1_RX_GPIO_PIN_NUM);
+    // Check if the pin for UART1 is what triggered the RX PORT interrupt
+    if ((GPIO_GetPinsInterruptFlags(UART1_RX_GPIO_BASE) & interrupt_flag) && s_pin_irq_func)
+    {
+        s_pin_irq_func();
+        GPIO_ClearPinsInterruptFlags(UART1_RX_GPIO_BASE, interrupt_flag);
+    }
+}
+
+void enable_autobaud_pin_irq(pin_irq_callback_t func)
+{
+    s_pin_irq_func = func;
+    // Only look for a falling edge for our interrupts
+    GPIO_SetPinInterruptConfig(UART1_RX_GPIO_BASE, UART1_RX_GPIO_PIN_NUM, kGPIO_IntFallingEdge);
+    UART1_RX_GPIO_BASE->IMR |= (1U << UART1_RX_GPIO_PIN_NUM);
+    NVIC_SetPriority(UART1_RX_GPIO_IRQn, GPIO_IRQC_INTERRUPT_ENABLED_PRIORITY);
+    NVIC_EnableIRQ(UART1_RX_GPIO_IRQn);
+}
+
+void disable_autobaud_pin_irq(void)
+{
+    NVIC_DisableIRQ(UART1_RX_GPIO_IRQn);
+    NVIC_SetPriority(UART1_RX_GPIO_IRQn, GPIO_IRQC_INTERRUPT_RESTORED_PRIORITY);
+    GPIO_SetPinInterruptConfig(UART1_RX_GPIO_BASE, UART1_RX_GPIO_PIN_NUM, kGPIO_NoIntmode);
+    s_pin_irq_func = 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// EOF
+////////////////////////////////////////////////////////////////////////////////
--- apps/autobaud_imxrt1011/src/main.c
+++ apps/autobaud_imxrt1011/src/main.c
@@ -11,28 +11,135 @@
 #include "board.h"
 #include "pin_mux.h"
 #include "clock_config.h"
+#include "autobaud.h"
+#include "microseconds.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
 
+#ifndef FREQ_392MHz
+#define FREQ_392MHz (392UL * 1000 * 1000)
+#endif
+
+enum
+{
+    kMaxIpgClock = 144000000UL,
+};
 
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-
+extern void uart_pinmux_config(pinmux_type_t pinmux);
 
 /*******************************************************************************
  * Code
  ******************************************************************************/
+
+//! @brief Get PIT clock value
+uint32_t get_pit_clock(void)
+{
+    // Get PIT clock source
+    uint32_t ahbBusDivider = ((CCM->CBCDR & CCM_CBCDR_IPG_PODF_MASK) >> CCM_CBCDR_IPG_PODF_SHIFT) + 1;
+    uint32_t periphDivider = ((CCM->CSCMR1 & CCM_CSCMR1_PERCLK_PODF_MASK) >> CCM_CSCMR1_PERCLK_PODF_SHIFT) + 1;
+    return SystemCoreClock / ahbBusDivider / periphDivider;
+}
+
+void clock_setup(void)
+{
+    uint32_t clock_divider = 1;
+    uint32_t core_clock = 0;
+
+    CLOCK_SetXtal0Freq(CPU_XTAL_CLK_HZ);
+    core_clock = FREQ_392MHz / clock_divider;
+
+    // If clock is not configured, configure clock first, otherwise, just update SystemCoreClock
+    {
+        CCM_ANALOG->PLL_SYS_CLR = CCM_ANALOG_PLL_SYS_POWERDOWN_MASK;
+        while ((CCM_ANALOG->PLL_SYS & CCM_ANALOG_PLL_SYS_LOCK_MASK) == 0)
+        {
+        }
+        CCM_ANALOG->PLL_SYS_SET = CCM_ANALOG_PLL_SYS_BYPASS_MASK;
+
+        /*
+         * PLL_SYS / PLL 2 / PLL 528
+         * PFD0 = 396MHz
+         * PFD1 = 396MHz
+         * PFD2 = 500MHz
+         * PFD3 = 396MHz
+         */
+        CCM_ANALOG->PFD_528 =
+            (CCM_ANALOG->PFD_528 & (~(CCM_ANALOG_PFD_528_PFD0_FRAC_MASK | CCM_ANALOG_PFD_528_PFD1_FRAC_MASK |
+                                      CCM_ANALOG_PFD_528_PFD2_FRAC_MASK | CCM_ANALOG_PFD_528_PFD3_FRAC_MASK))) |
+            CCM_ANALOG_PFD_528_PFD0_FRAC(24) | CCM_ANALOG_PFD_528_PFD1_FRAC(24) | CCM_ANALOG_PFD_528_PFD2_FRAC(19) |
+            CCM_ANALOG_PFD_528_PFD3_FRAC(24);
+
+        CCM_ANALOG->PLL_USB1 =
+            CCM_ANALOG_PLL_USB1_DIV_SELECT(0) | CCM_ANALOG_PLL_USB1_POWER(1) | CCM_ANALOG_PLL_USB1_ENABLE(1);
+        while ((CCM_ANALOG->PLL_USB1 & CCM_ANALOG_PLL_USB1_LOCK_MASK) == 0)
+        {
+        }
+        CCM_ANALOG->PLL_USB1_SET = CCM_ANALOG_PLL_USB1_BYPASS_MASK;
+
+        /*
+         * PLL_USB / PLL 3 / PLL 480
+         * PFD0 = 247MHz
+         * PFD1 = 247MHz
+         * PFD2 = 332MHz
+         * PFD3 = 392MHz
+         */
+        CCM_ANALOG->PFD_480 =
+            (CCM_ANALOG->PFD_480 & (~(CCM_ANALOG_PFD_480_PFD0_FRAC_MASK | CCM_ANALOG_PFD_480_PFD1_FRAC_MASK |
+                                      CCM_ANALOG_PFD_480_PFD2_FRAC_MASK | CCM_ANALOG_PFD_480_PFD3_FRAC_MASK))) |
+            CCM_ANALOG_PFD_480_PFD0_FRAC(35) | CCM_ANALOG_PFD_480_PFD1_FRAC(35) | CCM_ANALOG_PFD_480_PFD2_FRAC(26) |
+            CCM_ANALOG_PFD_480_PFD3_FRAC(22);
+
+        uint32_t ipg_divider = (core_clock + kMaxIpgClock - 1) / kMaxIpgClock;
+
+        CCM->CBCDR =
+            (CCM->CBCDR & (~(CCM_CBCDR_PERIPH_CLK_SEL_MASK | CCM_CBCDR_AHB_PODF_MASK | CCM_CBCDR_IPG_PODF_MASK))) |
+            CCM_CBCDR_PERIPH_CLK_SEL(0) | CCM_CBCDR_AHB_PODF(clock_divider - 1) | CCM_CBCDR_IPG_PODF(ipg_divider - 1);
+
+        // LPUART clock configuration, peripheral clock 40MHz
+        CCM->CSCDR1 = (CCM->CSCDR1 & (~(CCM_CSCDR1_UART_CLK_SEL_MASK | CCM_CSCDR1_UART_CLK_PODF_MASK))) |
+                      CCM_CSCDR1_UART_CLK_PODF(1);
+
+        // Configure the clock source for core modules
+        CCM->CBCMR = ((CCM->CBCMR & ~(CCM_CBCMR_PRE_PERIPH_CLK_SEL_MASK)) | (CCM_CBCMR_PRE_PERIPH_CLK_SEL(1)));
+
+        // Finally enable PLLs
+        CCM_ANALOG->PLL_SYS &= ~CCM_ANALOG_PLL_SYS_BYPASS_MASK;
+        CCM_ANALOG->PLL_USB1 &= ~CCM_ANALOG_PLL_USB1_BYPASS_MASK;
+    }
+
+    SystemCoreClockUpdate();
+}
+
 /*!
  * @brief Main function
  */
 int main(void)
 {
     /* Init board hardware. */
-    BOARD_InitPins();
-    BOARD_BootClockRUN();
-    BOARD_InitDebugConsole();
+    //BOARD_InitPins();
+    //BOARD_BootClockRUN();
+    //SystemCoreClockUpdate();
+    clock_setup();
+
+    // PIT clock gate control ON
+    CLOCK_EnableClock(kCLOCK_Pit);
+    microseconds_init();
+    //microseconds_delay(5000000);
+    uart_pinmux_config(kPinmuxType_PollForActivity);
+    autobaud_init();
+
+    uint32_t baudrate;
+    while (autobaud_get_rate(&baudrate) != kStatus_Success);
+    autobaud_deinit();
+    uart_pinmux_config(kPinmuxType_Peripheral);
+
+    //BOARD_InitDebugConsole();
+    uint32_t uartClkSrcFreq = BOARD_DebugConsoleSrcFreq();
+    DbgConsole_Init(BOARD_DEBUG_UART_INSTANCE, baudrate, BOARD_DEBUG_UART_TYPE, uartClkSrcFreq);
 
     PRINTF("Autobaud test success\r\n");
 

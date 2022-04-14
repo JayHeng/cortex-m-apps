8ec6ea4d0b62e41e17a634c3548ce5714936dce8
Jay Heng <jie.heng@nxp.com>
2022-04-14 17:30:56 +0800

    apps/mbw_rt1170 #1 Initial project
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

---
+++ apps/mbw_imxrt1176/bsp/src/hardware_init.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+/*${header:start}*/
+#include "pin_mux.h"
+#include "board.h"
+#include "fsl_debug_console.h"
+#include "clock_config.h"
+/*${header:end}*/
+  
+/*${function:start}*/
+static void print_rt_clocks(void)
+{
+    PRINTF("Clock roots frequency (MHz):\n");
+    for(uint32_t i=0; i<4; i++)
+    {
+        uint32_t freq = CLOCK_GetRootClockFreq((clock_root_t)i);
+        freq /= 1000000;
+        char name[5] = {0};
+        *(uint32_t *)name = *(uint32_t *)(CCM->CLOCK_ROOT[i].RESERVED_1);
+        PRINTF("%4s: %d\n", name, freq);
+    }
+}
+
+void BOARD_InitHardware(void)
+{
+    BOARD_ConfigMPU();
+    BOARD_InitPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+
+    print_rt_clocks();
+}
+
+uint32_t COREMARK_GetTimerClockFreq(void)
+{
+    return CLOCK_GetRootClockFreq(kCLOCK_Root_M7_Systick);
+}
+/*${function:end}*/

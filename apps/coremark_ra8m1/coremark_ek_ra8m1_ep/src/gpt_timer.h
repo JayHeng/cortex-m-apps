cf08423bf1c6beace25eab6922469e11d77d43c1
Jay Heng <hengjie1989@foxmail.com>
2025-02-28 11:16:20 +0800

    apps/coremark_ra8m1 #10 update gpt drv
    - removed other timer and kept periodic_timer only
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- apps/coremark_ra8m1/coremark_ek_ra8m1_ep/src/gpt_timer.h
+++ apps/coremark_ra8m1/coremark_ek_ra8m1_ep/src/gpt_timer.h
@@ -27,8 +27,6 @@
 #define GPT_MAX_PERCENT          (100U)          /* Max Duty Cycle percentage */
 #define BUF_SIZE                 (16U)           /* Size of buffer for RTT input data */
 #define PERIODIC_MODE_TIMER      (1U)            /* To perform GPT Timer in Periodic mode */
-#define PWM_MODE_TIMER           (2U)            /* To perform GPT Timer in PWM mode */
-#define ONE_SHOT_MODE_TIMER      (3U)            /* To perform GPT Timer in ONE-SHOT mode */
 #define INITIAL_VALUE            ('\0')
 #define TIMER_UNITS_MILLISECONDS  (1000U)        /* timer unit in millisecond */
 #define CLOCK_TYPE_SPECIFIER      (1ULL)         /* type specifier */
@@ -43,23 +41,10 @@
 #endif
 
 #define PERIODIC_MODE	   (1U)                  /* To check status of GPT Timer in Periodic mode */
-#define PWM_MODE	       (2U)                  /* To check status of GPT Timer in PWM mode */
-#define ONE_SHOT_MODE      (3U)                  /* To check status of GPT Timer in oNE-SHOT mode */
-
-#define EP_INFO    "\r\nThe project initializes GPT module in Periodic, PWM or One-shot mode based on user input " \
-		"from the displayed menu options." \
-		"\r\nIn periodic mode, user can enter the time period within the permitted ranges to change "\
-		"the frequency of the user LED." \
-		"\r\nIn PWM mode, user can enter the duty cycle within the specified range to adjust the "\
-		"intensity of the user LED." \
-		"\r\nIn ONE SHOT mode, Output will be displayed on JlinkRTTViewer when timer expires.\r\n "
 
 /* Function declaration */
 fsp_err_t init_gpt_timer(timer_ctrl_t * const p_timer_ctl, timer_cfg_t const * const p_timer_cfg, uint8_t timer_mode);
 fsp_err_t start_gpt_timer (timer_ctrl_t * const p_timer_ctl);
-fsp_err_t set_timer_duty_cycle(uint8_t duty_cycle_percent);
-uint32_t  process_input_data(void);
 void deinit_gpt_timer(timer_ctrl_t * const p_timer_ctl);
-void print_timer_menu(void);
 
 #endif /* GPT_TIMER_H_ */

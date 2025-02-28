d5dbf21783c8a6ad2e0061ce7291eec494ca8d4c
Jay Heng <hengjie1989@foxmail.com>
2025-02-28 16:59:13 +0800

    apps/coremark_ra8m1 #14 run coremark - 3
    - Lower GPT clock freq in configuration.xml
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- apps/coremark_ra8m1/coremark_ek_ra8m1_ep/src/coremark/barebones/core_portme.c
+++ apps/coremark_ra8m1/coremark_ek_ra8m1_ep/src/coremark/barebones/core_portme.c
@@ -41,7 +41,7 @@ CORETIMETYPE barebones_clock() {
 	Use lower values to increase resolution, but make sure that overflow does not occur.
 	If there are issues with the return value overflowing, increase this value.
 	*/
-#define CLOCKS_PER_SEC (120000000)
+#define CLOCKS_PER_SEC (30000000)
 #define GETMYTIME(_t) (*_t=barebones_clock())
 #define MYTIMEDIFF(fin,ini) ((fin)-(ini))
 #define TIMER_RES_DIVIDER 1
